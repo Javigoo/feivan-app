9034ff66b95cc1153604cf4d1222b30e0ab8344e
javigoo <javierroiggregorio@gmail.com>
2021-06-04 00:58:41 +0200

    hello world & icon

---
+++ app/feivan/feivanApp.swift
@@ -0,0 +1,17 @@
+//
+//  feivanApp.swift
+//  feivan
+//
+//  Created by Javier Roig Gregorio on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct feivanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
